8c394637e0b17762b442710c56dd567e2552a7c3
Trplnr <87922281+Trioplane@users.noreply.github.com>
2026-05-21 11:10:42 +0800

    test

--- src/build.js
+++ src/build.js
@@ -1,11 +1,14 @@
 import * as core from "@actions/core"
 import * as github from "@actions/github"
+import * as fs from "fs"
 
 // read the beet.json to get name, version, output folder, dp folder, rp folder, etc...
 // build the project using beet
 // set the output to the name, version, output folder, dp folder, rp folder, etc...
 
 try {
+    
+
     core.info("ran build.js")
 } catch (error) {
     core.setFailed(error.message)
--- src/validate.js
+++ src/validate.js
@@ -1,11 +1,25 @@
 import * as core from "@actions/core"
 import * as github from "@actions/github"
+import * as fs from "fs"
+import * as path from "path"
 
 // check if theres a beet.json and requirements.txt
 // check if the requirements.txt has "beet"
 // if not, die
 
 try {
+    const BEET_DIR = core.getInput('beet-dir')
+    const PATH_TO = {
+        REQUIREMENTS: path.join("requirements.txt"),
+        BEET: path.join(PROJECT_DIR, "beet.json"),
+    }
+
+    core.info(PATH_TO.REQUIREMENTS)
+    core.info(PATH_TO.BEET)
+
+    core.info("Checking for requirements.txt")
+    // if (!fs.readFileSync())
+
     core.info("ran validate.js")
 } catch (error) {
     core.setFailed(error.message)
